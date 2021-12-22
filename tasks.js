3ff9966398c0c944491a75f214f77180bf0cfc2e
rimamohamedassaad <rimamohamadassaad@gmail.com>
2021-12-22 15:44:24 +0200

    edit

--- tasks.js
+++ tasks.js
@@ -9,7 +9,7 @@
  * @param  {string} name the name of the app
  * @returns {void}
  */
-function startApp(name){
+function startApp(name) {
   process.stdin.resume();
   process.stdin.setEncoding('utf8');
   process.stdin.on('data', onDataReceived);
@@ -17,7 +17,7 @@ function startApp(name){
   console.log("--------------------")
 }
 
-let tasks=['study', 'practice', 'code'];
+let tasks = ['study', 'practice', 'code'];
 
 /**
  * Decides what to do depending on the data that was received
@@ -35,30 +35,33 @@ let tasks=['study', 'practice', 'code'];
  * @returns {void}
  */
 function onDataReceived(text) {
-  
+
   if (text === 'quit\n' || text === 'exit\n') {
     quit();
   }
-  else 
-    if((text.slice(0,5) === 'hello')){
-       hello(text.slice(5));
+  else
+    if ((text.slice(0, 5) === 'hello')) {
+      hello(text.slice(5));
     }
-  
-  else if(text === 'help\n'){
-    help();
-  }
-  else if(text === 'list\n'){
-    list();
-  }
-  else if((text.slice(0,3) === 'add')){
-    add(text);
-  }
-  else if(((text.slice(0,6) === 'remove'))){
-    remove(text);
-  }
-  else{
-    unknownCommand(text);
-  }
+
+    else if (text === 'help\n') {
+      help();
+    }
+    else if (text === 'list\n') {
+      list();
+    }
+    else if ((text.slice(0, 3) === 'add')) {
+      add(text);
+    }
+    else if (((text.slice(0, 6) === 'remove'))) {
+      remove(text);
+    }
+    else if (((text.slice(0, 4) === 'edit'))) {
+      edit(text)
+    }
+    else {
+      unknownCommand(text);
+    }
 }
 
 
@@ -70,8 +73,8 @@ function onDataReceived(text) {
  * @param  {string} c the text received
  * @returns {void}
  */
-function unknownCommand(c){
-  console.log('unknown command: "'+c.trim()+'"')
+function unknownCommand(c) {
+  console.log('unknown command: "' + c.trim() + '"')
 }
 
 
@@ -81,87 +84,113 @@ function unknownCommand(c){
  * @param {string} x
  * @returns {void}
  */
-function hello(x){
-  if(x.length<5){
+function hello(x) {
+  if (x.length < 5) {
     console.log('hello!')
   }
   else {
-  let tr = x.trim();
-  console.log('hello ' + tr.replace("\n","") + '!')
-}
+    let tr = x.trim();
+    console.log('hello ' + tr.replace("\n", "") + '!')
+  }
 }
 /**add tasks
  *
  * @param {string} x
  * @returns {void}
  */
- function add(x){
-  
-  if(x.slice(3).trim() == "" ){
+function add(x) {
+
+  if (x.slice(3).trim() == "") {
     console.log('error you must add a task')
   }
   else {
     tasks.push(x.slice(3).trim())
-    for(let i=0 ;i<tasks.length; i++){
-      console.log(i+1 +" : " + tasks[i]);
+    for (let i = 0; i < tasks.length; i++) {
+      console.log(i + 1 + " : " + tasks[i]);
     }
   }
 }
+/**edit tasks
+ *
+ * @param {string} x
+ * @returns {void}
+ */
+function edit(x) {
+
+  if (x.trim().slice(4) == "")
+    console.log('error');
+  else {
+    let arr = x.split(" ")
+
+    for (let i = 0; i < tasks.length + 1; i++) {
+      if (arr[1] == i) {
+        let newarray = arr.splice(2).join(' ').trim();
+        tasks.splice(i - 1, 1, newarray)
+      }
+      
+    }
+
+  }
+  if(isNaN(x.split(" ")[1])){
+  tasks[tasks.length -1]= (x.slice(5));
+  }
+
+}
+
 /**remove tasks
  *
  * @param {string} x
  * @returns {void}
  */
- function remove(x){
-  if(x.slice(6).trim() == "" ){
-    console.log( "the last task is removed :" + tasks.pop()+"\ntype list to see the new list");
-   }
-   else {
+function remove(x) {
+  if (x.slice(6).trim() == "") {
+    console.log("the last task is removed :" + tasks.pop() + "\ntype list to see the new list");
+  }
+  else {
     let arr = x.split(" ");
-    if(arr[1] > tasks.length){
+    if (arr[1] > tasks.length) {
       console.log('task not found')
     }
-    for(let i=0;i<tasks.length;i++){
-      if(arr[1] == (i+1) +'\n')
-      {
-        tasks.splice(tasks[i],1)
+    for (let i = 0; i < tasks.length; i++) {
+      if (arr[1] == (i + 1) + '\n') {
+        tasks.splice(tasks[i], 1)
         numb = i;
-        console.log('you removed the task number ' + (numb+1))
+        console.log('you removed the task number ' + (numb + 1))
       }
-     
+
+    }
   }
-      
-    } 
-  }
-    
-     
-    
-    
-  
-  
+}
+
+
+
+
+
+
 
 /**
  * list all the tasks
  *
  * @returns {void}
  */
-function list(){
-  for(let i=0 ;i<tasks.length; i++){
-    console.log(i+1 +" : " + tasks[i]);
-  }}
- /**
+function list() {
+  for (let i = 0; i < tasks.length; i++) {
+    console.log(i + 1 + " : " + tasks[i]);
+  }
+}
+/**
  
-  
+ 
 
 
 
 
 /**
- * Exits the application
- *
- * @returns {void}
- */
-function quit(){
+* Exits the application
+*
+* @returns {void}
+*/
+function quit() {
   console.log('Quitting now, goodbye!')
   process.exit();
 }
@@ -170,8 +199,8 @@ function quit(){
  * this function lists all the possible commands
  * @returns {void}
  */
-function help(){
-  console.log('hello : this command greeting you for example if you wirte hello rima the answer would be hello rima!\nquit or exit : to exit\nhelp : show all the command\nlist : list all the tasks\nadd : add tasks1 to add the tasks one to the list of tasks\nremove : to remove the last task\nremove 1 : to remove the first task\nremove 2 : to remove the second task')
+function help() {
+  console.log('hello : this command greeting you for example if you wirte hello rima the answer would be hello rima!\nquit or exit : to exit\nhelp : show all the command\nlist : list all the tasks\nadd : add tasks1 to add the tasks one to the list of tasks\nremove : to remove the last task for example\nremove 1 : to remove the first task\nremove 2 : to remove the second task\nedit : to edit tasks if you did not define the tasks number last task will delete')
 }
 // The following line starts the application
 startApp("Rima Assaad")
